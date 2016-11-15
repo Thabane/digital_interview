951fa382a7b98f689dd1a5c1e64a4d284abc97ca
Thabane Ndaba <skinnybonafide@yahoo.com>
2016-11-15 11:28:12 +0200

    Scaf Changes

--- Digital_Interview/UI/App_Start/RouteConfig.cs
+++ Digital_Interview/UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Resources", action = "SelectSubscription", id = UrlParameter.Optional }
+                defaults: new { controller = "ToDoLists", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
